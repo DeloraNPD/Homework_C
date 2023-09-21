d0c7bd33c025c9bc506a9c330a7a04eea0d700ff
Полина Наумова <polina@MacBook-Air-Polina.local>
2023-09-21 17:51:07 +0300

    Exercise 6

---
+++ HWC6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int even_number = number % 2;
+
+if (even_number == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
